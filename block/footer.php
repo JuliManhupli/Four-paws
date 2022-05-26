01faa409ef2720963364212f3b9e96671c87e8ee
JuliZliuka <juliamangup@gmail.com>
2022-05-26 14:43:10 +0300

    Create footer.php

---
+++ block/footer.php
@@ -0,0 +1,33 @@
+<footer>
+    <div class="banner">
+        <div class="background_footer">
+            <div class="container">
+                <div class="footer">
+                    <div>
+                        <blockquote>+38-050-146-7881</blockquote>
+                        <blockquote>KhersonHelp@yahoo.com</blockquote>
+                        <blockquote>Вулиця 49 Херсонської<br>Гвардійської дивізії, будинок 2</blockquote>
+                    </div>
+
+                    <?php
+                    $social_network = array("Facebook"=>"https://www.facebook.com/Kherson.Help.4paws/",
+                        "Instagram"=>"https://www.instagram.com/4lapu_herson/")?>
+                    <div  class="social_network">
+                        <a href="<?php echo $social_network['Facebook']?>">
+                            <div class="facebook">
+                                <img src="pictures/footer/Facebook.svg" alt="Facebook">
+                            </div>
+                        </a>
+                        <a href="<?php echo $social_network['Instagram']?>">
+                            <div>
+                                <img src="pictures/footer/Instagram.svg" alt="Instagram">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
