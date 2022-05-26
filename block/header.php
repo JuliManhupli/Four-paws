2b7a40f623a8eb626361a543b29a3ff1efeda6ba
JuliZliuka <juliamangup@gmail.com>
2022-05-26 14:43:15 +0300

    Create header.php

---
+++ block/header.php
@@ -0,0 +1,19 @@
+<div class="banner">
+    <div class="intro2">
+        <header>
+            <div class="container">
+                <div class="header_inner">
+                    <img src="pictures/logo.svg" alt="logo">
+                    <nav>
+                        <a href="?page=landing">Головна</a>
+                        <a href="?page=pets">Пошук улюбленця</a>
+                        <a href="?page=help">Як допомогти</a>
+                        <a href="?page=contacts">Контакти</a>
+                    </nav>
+                    <blockquote>+38-050-146-7881</blockquote>
+                    <img class="menu" src="pictures/menu.svg" alt="Menu">
+                </div>
+            </div>
+        </header>
+    </div>
+</div>
