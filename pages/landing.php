3bd82eaba4d2de508e39689d9bae1a99396cf7b9
JuliZliuka <juliamangup@gmail.com>
2022-05-26 14:43:34 +0300

    Create landing.php

---
+++ pages/landing.php
@@ -0,0 +1,213 @@
+<div class="banner">
+    <section class="intro">
+        <header>
+            <div class="container">
+                <div class="header_inner">
+                    <img src="pictures/logo.svg" alt="logo">
+                    <nav>
+                        <a href="?page=landing">Головна</a>
+                        <a href="?page=pets">Пошук улюбленця</a>
+                        <a href="?page=help">Як допомогти</a>
+                        <a href="?page=contacts">Контакти</a>
+                    </nav>
+                    <blockquote>+38-050-146-7881</blockquote>
+                    <img class="menu" src="pictures/menu.svg" alt="Menu">
+                </div>
+
+                <div class="tagline">
+                    <h1>Подаруй турботу тим,<br>хто цього потребує</h1>
+                    <h2>Притулок для кілька сотень тварин</h2>
+                    <div class="col-6 col-lg-4 col-12">
+                        <button onClick=location.href="?page=pets">Взяти тваринку</button>
+                    </div>
+                </div>
+
+            </div>
+        </header>
+    </section>
+</div>
+
+
+<div class="container">
+    <section class="project">
+        <h1>Про проєкт</h1>
+        <div class="description">
+            <blockquote>
+                <b>Чотири лапи</b> – це громадська організація, яка існує завдяки небайдужим людям.<br>
+                Ми рятуємо тварин у місті Херсоні, котрі потребують нашої допомоги.<br>
+                Наша мета - зменшити кількість безпритульних тварин і зробити їх життя щасливішим.<br>
+            </blockquote>
+        </div>
+    </section>
+</div>
+
+
+
+<div class="banner">
+    <section class="background">
+        <div class="container">
+            <!Обери свого улюбленця>
+            <h1>Обери свого улюбленця</h1>
+            <div class="choose_pet">
+                <div class="row">
+                    <div class="col-lg-4 col-12">
+                        <a href="?page=page">
+                            <div class="pet">
+                                <img src="pictures/pets_lending/Belle.png" alt="Belle">
+                                <div>
+                                    <h2>Ізабель</h2>
+                                    <blockquote>2 роки, дівчинка</blockquote>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-lg-4 col-12">
+                        <a href="?page=page">
+                            <div class="pet">
+                                <img src="pictures/pets_lending/Myachik.png" alt="Myachik">
+                                <div>
+                                    <h2>М'ячик</h2>
+                                    <blockquote>5 років, хлопчик</blockquote>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-lg-4 col-12">
+                        <a href="?page=page">
+                            <div class="pet">
+                                <img src="pictures/pets_lending/Bublik.png" alt="Bublik">
+                                <div>
+                                    <h2>Бублик</h2>
+                                    <blockquote>3 роки, хлопчик</blockquote>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-4"></div>
+                    <div class="col-lg-4 col-12">
+                        <button onClick=location.href="?page=pets">Показати більше</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+<div class="container">
+    <section class="reasons">
+        <h1>Причини взяти тварину з притулку</h1>
+        <div class="row">
+            <div class="col-lg-6 col-12">
+                <div class="reason">
+                    <img src="pictures/reasons/1.svg" alt="Heard in hand">
+                    <blockquote>
+                        Ви рятуєте маленькі життя
+                    </blockquote>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-12">
+                <div class="reason another_color">
+                    <img src="pictures/reasons/2.svg" alt="Cat">
+                    <blockquote>
+                        Ви подаєте приклад іншим
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6 col-12">
+                <div class="reason another_color med">
+                    <img src="pictures/reasons/3.svg" alt="Medicine chest">
+                    <blockquote>
+                        Усі тварини проходять<br>медичне обстеження
+                    </blockquote>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-12">
+                <div class="reason heard">
+                    <img src="pictures/reasons/4.svg" alt="Heard">
+                    <blockquote>
+                        Тварини покращують<br>емоційний стан
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+<div class="container">
+    <section class="reviews">
+        <h1>Відгуки</h1>
+        <div class="review">
+            <div class="arrow1 arrow">
+                <img src="pictures/reviews/arrow1.svg" alt="Arrow left">
+            </div>
+            <div class="review_text">
+                <img src="pictures/reviews/Phil.png" alt="Phil">
+                <div class="text">
+                    <h2>Тетяна</h2>
+                    <blockquote>
+                        Хочу висловити Вам величезну вдячність за мого нового друга на прізвисько Філя. Котик доглянутий, зроблено всі щеплення. Тепер після роботи мене вечорами чекає м'яке муркотливе диво! У будинку відразу стало тепліше та затишніше!
+                    </blockquote>
+                </div>
+            </div>
+            <div class="arrow2 arrow">
+                <img src="pictures/reviews/arrow2.svg" alt="Arrow right">
+            </div>
+        </div>
+    </section>
+</div>
+
+
+<div class="container">
+    <section class="another_help">
+        <h1>Як ще допомогти?</h1>
+
+        <div class="row">
+            <div class="col-lg-4 col-12">
+                <div class="path_help">
+                    <img src="pictures/another_help/1.svg" alt="Heard">
+                    <blockquote>
+                        Пожертви
+                    </blockquote>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-12">
+                <div class="path_help another_color">
+                    <img src="pictures/another_help/2.svg" alt="Heard">
+                    <blockquote>
+                        Допомога волонтерів
+                    </blockquote>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-12">
+                <div class="path_help">
+                    <img src="pictures/another_help/3.svg" alt="Heard">
+                    <blockquote>
+                        Тимчасова перетримка
+                    </blockquote>
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="row">
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4 col-12">
+                <button onClick=location.href="?page=help">Дізнатися більше</button>
+            </div>
+        </div>
+    </section>
+</div>
